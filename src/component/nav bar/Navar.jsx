9183e1bcceb95831d89ccfbdb7c78a7c81b22bd5
Don-kay <daviskrisa1@yahoo.com>
2023-04-04 10:27:10 +0100

    Little responsive change

--- src/component/nav bar/Navar.jsx
+++ src/component/nav bar/Navar.jsx
@@ -1,19 +1,53 @@
 import React from "react";
-import { useState, useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { navigate } from "../../data component/data";
 import { BsFillCaretDownFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { AiOutlineClose } from "react-icons/ai";
 import Logo from "../../assets/daniellasLogo.png";
-import {
-  FaFacebookF,
-  FaFacebookMessenger,
-  FaInstagram,
-  FaWhatsapp,
-} from "react-icons/fa";
 import "./Navar.css";
+import {
+  Nav_Cont,
+  Detail_Kerry,
+  Contact,
+  Ctncont,
+  Socials,
+  Faceboook,
+  Messenger,
+  Instagram,
+  Whatsapp,
+  Info_cont,
+  Info,
+  Logo_icon,
+  NavG,
+  Portal_Btn,
+  Page,
+  Page_sub,
+  Portal_Btn_link,
+  Mobile,
+  Hamburger,
+  Closburger,
+  Main_Map,
+  SubCont,
+  Navel,
+  NavG1,
+  Infos,
+  FaMobiles,
+  NewHamburger,
+} from "../../Responsive/Responsive_Style";
 import { useGlobalContext } from "../context/context";
 
 const Navar = () => {
-  const { openSubMenu, closeSubMenu } = useGlobalContext();
+  const {
+    closeSubMenu,
+    openSidebar,
+    closeSidebar,
+    isSidebarOpen,
+    DisplayDropDown,
+    subitems,
+    setSubitems,
+  } = useGlobalContext();
+
   useEffect(() => {
     const navbar = document.querySelector(".navsec");
     const page = document.querySelector(".page");
@@ -42,84 +76,128 @@ const Navar = () => {
     sectionOneObserver.observe(menu);
   }, []);
 
-  // window.onscroll = function () {
-  //   if (window.pageYOffset >= navbar.offsetTop) {
-  //     navbar.classList.add("nav-sticky");
-  //   } else {
-  //     navbar.classList.remove("nav-sticky");
-  //   }
-  // };
-  const handleSubmenu = (e) => {
-    if (!e.target.classList.contains("page")) {
-      closeSubMenu();
+  const closeBar = () => {
+    setSubitems(null);
+  };
+  const Close = (e) => {
+    if (!e.target.classList.contains("subCont")) {
+      setSubitems(null);
     }
   };
-  const DisplayMenu = (e) => {
-    const page = e.target.textContent;
-    const object = e.target.getBoundingClientRect();
-    // console.log(object);
-    const center = (object.right + object.x) / 2;
-    const bottom = object.bottom - 3;
-    openSubMenu(page, { center, bottom });
-  };
+
   return (
-    <main className="navcont">
-      <div className="detailskerry">
-        <div className="socials">
-          <a href="" className="href">
-            <FaFacebookF className="socio" />
-          </a>
-          <a href="https://m.me/david.kerry.370" className="href">
-            <FaFacebookMessenger className="socio" />
-          </a>
-          <a href="" className="href">
-            <FaInstagram className="socio" />
-          </a>
+    <Nav_Cont>
+      <Detail_Kerry className="detailskerry">
+        <Contact>
+          <FaMobiles />
+          <Ctncont>
+            <Mobile className="mobile">08023438285</Mobile>
+            <Mobile className="mobile">08137375628</Mobile>
+          </Ctncont>
+        </Contact>
+        <Socials>
           <a
-            href="https://api.whatsapp.com/send?phone=+23408052729364"
+            href="https://www.facebook.com/Daniellasmontessorischool/"
+            target={"_blank"}
+            rel="noreferrer"
             className="href"
           >
-            <FaWhatsapp className="socio" />
+            <Faceboook className="socio" />
           </a>
-        </div>
-        <div className="infoCont">
-          <h1 className="info">
-            <span className="enquire">
-              <h3>for enquiries:</h3>
-            </span>
-            <a href="mailto:dkrizlive@gmail.com" id="info">
-              dkrizlive@gmail.com
-            </a>
-          </h1>
-        </div>
-      </div>
-      <section onMouseOver={handleSubmenu} className="navsec  nav-sticky">
-        <img src={Logo} alt="logo1" className="logo" />
-        <button onMouseOver={closeSubMenu} className="portal">
-          Portal
-        </button>
-        {navigate.map((navs, id) => {
-          return (
-            <nav className="navg">
-              <ul className="shadowUl">
-                <li key={id} className="navbar">
-                  <div className="navel">
+          <a
+            href="https://m.me/daniellasmontessorischool"
+            rel="noreferrer"
+            target={"_blank"}
+            className="href"
+          >
+            <Messenger className="socio" />
+          </a>
+          <a href="i.me" className="href">
+            <Instagram className="socio" />
+          </a>
+          <a
+            href="https://api.whatsapp.com/send?phone=+2348023438285"
+            className="href"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <Whatsapp className="socio" />
+          </a>
+        </Socials>
+        <Info_cont>
+          <Info>
+            <span className="enquire">for enquiries:</span>
+            <Infos href="mailto:daniellasmontessorischool@gmail.com">
+              daniellasmontessorischool@gmail.com
+            </Infos>
+          </Info>
+        </Info_cont>
+      </Detail_Kerry>
+      <section className="navsec  nav-sticky">
+        <Link to={"/"}>
+          <Logo_icon src={Logo} alt="logo1" />
+        </Link>
+
+        {/* <NewHamburger></NewHamburger> */}
+        {isSidebarOpen ? (
+          <Closburger className="hamburger" onClick={closeSidebar} />
+        ) : (
+          <Hamburger className="hamburger" onClick={openSidebar} />
+        )}
+
+        <NavG1>
+          {navigate.map((navs) => {
+            const { id, pages, address, link } = navs;
+            return (
+              <Main_Map>
+                <NavG>
+                  <Navel>
                     <BsFillCaretDownFill className="menu-icon" />
-                    <h1 onMouseOver={DisplayMenu} key={id} className="page">
-                      {navs.pages}
-                    </h1>
-                  </div>
-                  <h5 className="page-sub">{navs.address}</h5>
-                </li>
-              </ul>
-            </nav>
-          );
-        })}
+                    <Page onMouseOver={() => DisplayDropDown(id)}>
+                      {pages}
+                    </Page>{" "}
+                    <Page_sub>{address}</Page_sub>
+                  </Navel>
+                  <SubCont onMouseLeave={closeBar}>
+                    {link.map((links) => {
+                      const { idx, label, url } = links;
+                      return (
+                        <section
+                          className={
+                            subitems === id ? "subMenu" : "subMenuAnimate"
+                          }
+                          onClick={closeBar}
+                          key={idx}
+                        >
+                          <Link style={{ textDecoration: "none" }} to={url}>
+                            <h3
+                              key={id}
+                              className={
+                                subitems === id ? "dropdown" : "closeDisplay"
+                              }
+                            >
+                              {label}
+                            </h3>
+                          </Link>
+                        </section>
+                      );
+                    })}
+                  </SubCont>
+                </NavG>
+              </Main_Map>
+            );
+          })}
+        </NavG1>
+        <Portal_Btn onMouseOver={closeSubMenu}>
+          <Portal_Btn_link href="https://studentdashboard.daniellasmontessorischool.com.ng/student">
+            Portal
+          </Portal_Btn_link>
+        </Portal_Btn>
         {/* <div className="shadowcont"></div>
         <div className="shadowcontfix"></div> */}
         {/* <div onMouseOver={closeSubMenu} className="shadowcontfix1"></div> */}
       </section>
-    </main>
+    </Nav_Cont>
   );
 };
 
